f358cef104fc6dc4506862c61aef8525b65320fc
Tim <timkentdev@gmail.com>
2026-09-02 13:16:26 -0700

    remove mocked data

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,14 +1,8 @@
 import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll, vi } from "vitest";
+import { afterEach, vi } from "vitest";
 import "@testing-library/jest-dom";
-import { server } from "./mocks";
-
-beforeAll(() => server.listen());
 
 afterEach(() => {
-	server.resetHandlers();
 	cleanup();
 	vi.clearAllMocks();
 });
-
-afterAll(() => server.close());
